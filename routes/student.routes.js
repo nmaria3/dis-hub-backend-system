68590f8d60760371babba59be1666c001db99032
nmaria3 <marianankinga361@gmail.com>
2026-04-09 17:12:50 +0300

    Implement Backend Browse Functionality

--- routes/student.routes.js
+++ routes/student.routes.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { updateActivity } = require("../controllers/students.controller");
+const { getAllDissertations } = require("../controllers/dissertations.controller")
 const { requireAuth } = require("@clerk/express");
 
 router.get("/api/activity", requireAuth(), updateActivity);
+
+router.get("/get-dissertations",  getAllDissertations);
 
 module.exports = router;
