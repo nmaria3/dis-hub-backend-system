bea4428b7c9d0ad588d8836f74d9cbbf3c9c4a73
nmaria3 <marianankinga361@gmail.com>
2026-04-18 15:29:10 +0300

    Fixed the Uploads Folder issue 2

--- utils/CheckForUploadsFolder.js
+++ utils/CheckForUploadsFolder.js
@@ -2,11 +2,11 @@ const fs = require("fs");
 const path = require("path");
 
 function ensureUploadsFolder() {
-    const uploadsPath = path.join(__dirname, "uploads");
+    const uploadsPath = path.join(process.cwd(), "uploads");
 
     if (!fs.existsSync(uploadsPath)) {
         fs.mkdirSync(uploadsPath, { recursive: true });
-        console.log("Uploads folder created:", uploadsPath);
+        console.log("Uploads folder created at root:", uploadsPath);
     } else {
         console.log("Uploads folder already exists:", uploadsPath);
     }
